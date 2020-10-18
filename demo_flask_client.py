ad0395748ae16d6f1d160cf1132b73b856ac25e7
hansenc101 <christopher.hansen@eagles.oc.edu>
2020-10-18 00:14:57 -0500

    Fully functional, meeting requirements specified for Project_3

--- demo_flask_client.py
+++ demo_flask_client.py
@@ -3,50 +3,55 @@ import random
 from PyQt5 import QtWidgets,uic
 from PyQt5.QtCore import QThread
 import requests
-#r = requests.get('http://10.0.2.5:5000/time')
-#print (r.json())
 
-#r = requests.post('http://10.0.2.5:5000/set_text', json={"button":"OC", "status":"CENG 4113/5113"})
-#if r.ok:
-#    print (r.json())
-    #====================== New Code =========================================
 ah_Count = 0 # Number of ahs
 
 class FlaskClient(QThread):
     def run(self):
-        r = requests.post('http://10.0.2.5:5000/set_text', json={"button":"OC", "ahCount":str(ah_Count), "status":"CENG 4113/5113"})
+        r = requests.post('http://10.0.2.5:5000/set_text', json={"ahCount":str(ah_Count), "status":"CENG 4113/5113"}) # Set the initial values
         if r.ok:
-            print(r.json())
-        
+            print(r.json()) # output to console the current values
 
-def Quit():
+
+# Function to quit thread 
+def Quit(): 
     thread.terminate()
     thread.wait()
     App.quit()
 
+
+# Function that executes when Increment Button is clicked
 def Handle_Increment_Click():
     global ah_Count
-    ah_Count = ah_Count + 1
+    ah_Count = ah_Count + 1 # Increment the ah_Count by 1
 
-    UI.lblOutput.setText("Clicked " + str(ah_Count) + " times")
-    r = requests.post('http://10.0.2.5:5000/set_text', json={"button":"OC", "ahCount":str(ah_Count), "status":"CENG 4113/5113"})
+    UI.lblOutput.setText("Ah Count: " + str(ah_Count)) # Display the current ah_Count
+    r = requests.post('http://10.0.2.5:5000/set_text', json={"ahCount":str(ah_Count), "status":"CENG 4113/5113"}) # Send text data to web server
 
+
+# Function that executes when Decrement Button is clicked
 def Handle_Decrement_Click():
     global ah_Count
-    ah_Count = ah_Count - 1
+    ah_Count = ah_Count - 1 # Decrement the ah_Count by 1
+    if ah_Count == -1: # Do not let ah_Count go below 0
+        ah_Count = 0   # If ah_Count goes negative, just set to 0
 
-    BG_Color = "rgb(" + str(random.randint(0,255)) + "," + str(random.randint(0,255)) + "," + str(random.randint(0,255)) + ");"
-    FG_Color = "rgb(255,255,255);"
+    UI.lblOutput.setText("Ah Count: " + str(ah_Count)) # Display the current ah_Count
+    r = requests.post('http://10.0.2.5:5000/set_text', json={"ahCount":str(ah_Count), "status":"CENG 4113/5113"}) # Send text data to web server
 
-    UI.lblOutput.setText("Clicked " + str(ah_Count) + " times")
-    UI.lblOutput.setStyleSheet("QLabel {background-color :" + BG_Color + "color : " + FG_Color + "}")
-    r = requests.post('http://10.0.2.5:5000/set_text', json={"button":"OC", "ahCount":str(ah_Count), "status":"CENG 4113/5113"})
 
+# Function that executes when any of the color sliders change value
 def Handle_Color_Adjust():
-    BG_Color = "rgb(" + str(UI.redSlider.value()) + "," + str(UI.greenSlider.value()) + "," + str(UI.blueSlider.value()) + ");"
-    FG_Color = "rgb(255,255,255);"
-    UI.lblOutput.setStyleSheet("QLabel {background-color :" + BG_Color + "color : " + FG_Color + "}")
+    BG_Color = "rgb(" + str(UI.redSlider.value()) + "," + str(UI.greenSlider.value()) + "," + str(UI.blueSlider.value()) + ");" # Set Background rgb color to values from sliders
+    FG_Color = "rgb(255,255,255);" # Set foreground to white
+    UI.lblOutput.setStyleSheet("QLabel {background-color :" + BG_Color + "color : " + FG_Color + "}") # Set the colors of the QLabel widget using values from BG_Color and FG_Color
+    UI.redMagLabel.setText("Red:   " + str(UI.redSlider.value())) # output the red value for background rgb color
+    UI.greenMagLabel.setText("Green: " + str(UI.greenSlider.value())) # output the green value for background rgb color
+    UI.blueMagLabel.setText("Blue:  " + str(UI.blueSlider.value())) # output the blue value for background rgb color
+
+    # Send color data to web server
     r = requests.post('http://10.0.2.5:5000/set_color', json={"red":str(UI.redSlider.value()), "green":str(UI.greenSlider.value()) , "blue":str(UI.blueSlider.value())})
+
 
 App = QtWidgets.QApplication([]) # Instantiate the application
 UI=uic.loadUi("demo_flask_client.ui") # Load in client user interface: demo_flask_client.ui
@@ -54,11 +59,12 @@ UI=uic.loadUi("demo_flask_client.ui") # Load in client user interface: demo_flas
 UI.incrementBtn.clicked.connect(Handle_Increment_Click) # Connect Handle_Increment_Click to incrementBtn
 UI.decrementBtn.clicked.connect(Handle_Decrement_Click) # Connect Handle_Decrement_Click to decrementBtn
 UI.actionQuit.triggered.connect(Quit) # Quit the application
-UI.redSlider.valueChanged.connect(Handle_Color_Adjust)
-UI.greenSlider.valueChanged.connect(Handle_Color_Adjust)
-UI.blueSlider.valueChanged.connect(Handle_Color_Adjust)
+UI.redSlider.valueChanged.connect(Handle_Color_Adjust) # Connect Handle_Color_Adjust to redSlider
+UI.greenSlider.valueChanged.connect(Handle_Color_Adjust) # Connect Handle_Color_Adjust to greenSlider
+UI.blueSlider.valueChanged.connect(Handle_Color_Adjust) # Connect Handle_Color_Adjust to blueSlider
 
 UI.show() # Display the ui
+UI.lblOutput.setText("Ah Count: 0")
 
 thread = FlaskClient() # Instantiate the thread
 thread.start() # Begin the thread
--- demo_flask_server.py
+++ demo_flask_server.py
@@ -7,66 +7,67 @@ import flask
 import time
 
 class FlaskServer(QThread):
-    app = flask.Flask(__name__)
-    app.config["DEBUG"] = False
+    app = flask.Flask(__name__) # instantiate the flask application
+    app.config["DEBUG"] = False # Set DEBUG to False so others can access the web server
 
     def run(self):
-       self.app.run(host='0.0.0.0')
+       self.app.run(host='0.0.0.0') # Allow anyone on the network to connect to the web server
 
     @app.route('/', methods=['GET'])
     def Home():
-        return "<h1>Hello, World!</h1><p>This webserver is working!</p>"
+        return "<h1>Hello, World!</h1><p>This webserver is working!</p>" # Output that verifies the webserver is working
 
-    @app.route('/time', methods=['GET'])
+    @app.route('/time', methods=['GET']) # run Get_Time when the client requests to post to http://10.0.2.5:5000/time
+    # This function will return the current time from the web server
     def Get_Time():
-        t = time.localtime()
+        t = time.localtime() # get time from system
         current_time = time.strftime("%H:%M:%S", t)
         return flask.jsonify(Current_Time=current_time)
 
-    @app.route('/set_text', methods=['POST'])
-    def Set_Text():
-        print (flask.request.json)
-        UI.btnCalculate.setText(flask.request.json['button']) # Get the text for the field 'button'
+
+    @app.route('/set_text', methods=['POST']) # run Set_Text when the client requests to post to http://10.0.2.5:5000/set_text
+    # This function will update the text fields for the server gui
+    def Set_Text(): 
+        print (flask.request.json) 
         UI.statusbar.showMessage(flask.request.json['status']) # Get the text for the field 'status'
         UI.lblOutput.setText("Ah Count: " + flask.request.json['ahCount']) # Get the text for the field 'ahCount'
-        return flask.jsonify(flask.request.json)
+        return flask.jsonify(flask.request.json) # return json object
 
-    @app.route('/set_color', methods=['POST'])
+    @app.route('/set_color', methods=['POST']) # run Set_Color when the client requests to post to http://10.0.2.5:5000/set_color
+    # This function will update the lblOutput colors, based upon the values set by the client
     def Set_Color():
-        BG_Color = "rgb(" + str(flask.request.json['red']) + "," + str(flask.request.json['green']) + "," + str(flask.request.json['blue']) + ");"
-        FG_Color = "rgb(255,255,255);"
+        redColorValue = flask.request.json['red'] # Get the current red rgb value from client
+        greenColorValue = flask.request.json['green'] # Get the current green rgb value from client
+        blueColorValue = flask.request.json['blue'] # Get the current blue rgb value from client
+        BG_Color = "rgb(" + str(redColorValue) + "," + str(greenColorValue) + "," + str(blueColorValue) + ");" # set the background variable to the values read in from client
+        FG_Color = "rgb(255,255,255);" # set the foreground variable to these values
+        UI.lblOutput.setStyleSheet("QLabel {background-color :" + BG_Color + "color : " + FG_Color + "}") # Set the colors of the QLabel widget using values from BG_Color and FG_Color
+        UI.redMagLabel.setText("Red:   " + str(redColorValue)) # Output the current value of red rgb background color from client
+        UI.greenMagLabel.setText("Green: " + str(greenColorValue)) # Output the current value of green rgb background color from client
+        UI.blueMagLabel.setText("Blue:  " + str(blueColorValue)) # Output the current value of blue rgb background color from client
+        return flask.jsonify(flask.request.json) # return json object
 
-        UI.lblOutput.setStyleSheet("QLabel {background-color :" + BG_Color + "color : " + FG_Color + "}")
-        return flask.jsonify(flask.request.json)
 
+ah_Count = 0 # This will hold the current ah_Count
+redColorValue = 0 # This will hold the current value of red for the rgb background color
+greenColorValue = 0 # This will hold the current value of green for the rgb background color
+blueColorValue = 0 # This will hold the current value of blue for the rgb background color
 
-Click_Count = 0
-
+# Function to stop the thread and quit the application
 def Quit():
     thread.terminate()
     thread.wait()
     App.quit()
 
-def Handle_Click():
-    global Click_Count
-    Click_Count = Click_Count + 1
 
-    BG_Color = "rgb(" + str(random.randint(0,255)) + "," + str(random.randint(0,255)) + "," + str(random.randint(0,255)) + ");"
-    FG_Color = "rgb(255,255,255);"
+App = QtWidgets.QApplication([]) # instantiate application
+UI=uic.loadUi("demo_flask_server.ui") # load in server ui
 
-    UI.lblOutput.setText("Clicked " + str(Click_Count) + " times")
-    UI.lblOutput.setStyleSheet("QLabel {background-color :" + BG_Color + "color : " + FG_Color + "}")
+UI.actionQuit.triggered.connect(Quit) # connect quit function to action actionQuit
 
-App = QtWidgets.QApplication([])
-UI=uic.loadUi("demo_flask_server.ui")
+UI.show() # Display the user interface
+UI.lblOutput.setText("Ah Counter Disconnected") # Set the initial text in lblOutput to indicate no client is connected
+thread = FlaskServer() # instantiate a thread of FlaskServer
+thread.start() # Begin processing the thread
 
-UI.btnCalculate.clicked.connect(Handle_Click)
-UI.actionCalculate.triggered.connect(Handle_Click)
-UI.actionQuit.triggered.connect(Quit)
-
-UI.show()
-
-thread = FlaskServer()
-thread.start()
-
-sys.exit(App.exec_())
+sys.exit(App.exec_()) # Exit the program
